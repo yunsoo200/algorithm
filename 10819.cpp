f7a39be0d8925e82302090082a995baec1be12b5
yunsoo <yunsoo200@naver.com>
2019-10-04 23:53:27 +0900

    조세퍼스

---
+++ 10819.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	int arr[8];
+	for (int i = 0; i < n; i++) {
+		int tmp; cin >> tmp;
+		arr[i] = tmp;
+	}
+}
---
+++ 1158.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+using namespace std;
+
+class circleList;
+
+class Node {
+public:
+	int data;
+	Node* next;
+
+	Node() { data = 0; next = 0; }
+	Node(int data) { this->data = data; next = 0; }
+
+	friend circleList;
+};
+
+class circleList {
+public:
+	Node* head;
+	Node* cursor;
+
+	circleList() { head = 0; cursor = 0; }
+
+	void insert(Node* tmp) {
+		if (head == 0) {
+			head = tmp;
+			tmp->next = head;
+			cursor = head;
+			return;
+		}
+
+		cursor->next = tmp;
+		tmp->next = head;
+		cursor = tmp;
+	}
+
+	void moveCursor() {
+		cursor = head;
+	}
+
+	int pop(int index) {
+		Node* now = new Node();
+
+		while (index != 1) {
+			now = cursor;
+			cursor = cursor->next;
+			index--;
+		}
+		if (index != 1)
+			now->next = cursor->next;
+		Node* tmp = cursor;
+
+		int result = tmp->data;
+		if (tmp == head)
+			head = head->next;
+		cursor = cursor->next;
+		delete(tmp);
+		return result;
+	}
+};
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	circleList c;
+
+	for (int i = 1; i <= n; i++) {
+		c.insert(new Node(i));
+	}
+
+	c.moveCursor();
+
+	cout << "<";
+	for (int i = 1; i <= n; i++) {
+		cout << c.pop(m);
+		if (i != n)
+			cout << ", ";
+
+	}
+	cout << ">" << endl;
+	return 0;
+}
